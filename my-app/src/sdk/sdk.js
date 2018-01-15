3e311ffec8d7ae511fc4281d6b0639374eb3e9e8
guyy <guy.yomtov@netapp.com>
2018-01-15 12:27:57 +0200

    add login+signup

--- my-app/src/sdk/sdk.js
+++ my-app/src/sdk/sdk.js
@@ -3,7 +3,7 @@
  */
 import axios from 'axios';
 
-axios.defaults.baseURL = 'http://localhost:8080/';
+axios.defaults.baseURL = 'http://localhost:5000/';
 axios.defaults.headers.post['Content-Type'] = 'application/json';
 axios.defaults.headers.put['Content-Type'] = 'application/json';
 
