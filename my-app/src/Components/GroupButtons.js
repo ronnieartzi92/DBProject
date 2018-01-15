b46a3fd490c260747cccd60d3be91c7a4a700f31
guyy <guy.yomtov@netapp.com>
2018-01-15 15:16:15 +0200

    fix search

--- my-app/src/Components/GroupButtons.js
+++ my-app/src/Components/GroupButtons.js
@@ -1,3 +1,29 @@
 /**
  * Created by guyy on 1/15/2018.
  */
+import React from 'react'
+import { Button } from 'semantic-ui-react'
+
+
+export default class GroupButtons extends Component {
+    constructor(props, context) {
+        super(props, context);
+        this.state = {options: this.props.options};
+    };
+    componentWillReceiveProps(nextProps){
+        this.setState({options: nextProps.options});
+    }
+
+
+    render() {
+
+        return (
+            <Button.Group>
+            {this.state.options.map((item, index) => {
+                return <><Button onClick={this.props.chooseOption.bind(this, item)}>One</Button><Button.Or /></>
+            })}
+            </Button.Group>
+        )
+    }
+
+}
