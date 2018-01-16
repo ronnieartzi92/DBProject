0f3605354762d708fedb77109a0fa82f08245373
Mike Moldawsky <mike.moldawsky@gmail.com>
2018-01-16 16:00:50 +0200

    not UTF 8

--- db/scripts/mysql/fill_database.py
+++ db/scripts/mysql/fill_database.py
@@ -110,7 +110,10 @@ class MysqlScripts:
         try:
             obj.insert(cursor)
         except Exception as e:
-            MysqlScripts.print_entity(e, obj)
+            try:
+                MysqlScripts.print_entity(e, obj)
+            except Exception as e:
+                print("############## failed printing all object fields...... ################")
         # closing resources
         cnx.commit()
         cursor.close()
@@ -118,9 +121,12 @@ class MysqlScripts:
 
 
 if __name__ == "__main__":
-    d = {"description": "\u201cCloser\u201d is a millennial romance anthem that celebrates youth and heartbreak. It features vocals from singer-songwriter Halsey and Chainsmokers member Andrew Taggart, marking the first time The Chainsmokers sung on their own track and the first time they\u2019ve collaborated with Halsey.\n\nOn Twitter, the duo wrote about the meaning of the song:\n\nThis song is dedicated to anyone that hooked up with their EX and right after remember all the reasons why they broke up.\n\n\u201cCloser\u201d was premiered at Bonnaroo by Halsey and she later confirmed its release on her Instagram:\n\n\"You heard it tonight. My phone hasn\u2019t had service in 2 days, bonnaroo but I\u2019m pullin enough juice to inform you that @thechainsmokers and I have a BRAND NEW SONG coming out soon. Those of you who got to witness it tonight, lucky you. \ud83d\udc8b\ud83d\udc8b\ud83d\udc8b\"\n\nHalsey also teased the track on Twitter a week prior to the release by releasing a cropped version of the photo featured the single\u2019s art. <a href=\"http://www.last.fm/music/The+Chainsmokers/_/Closer\">Read more on Last.fm</a>. User-contributed text is available under the Creative Commons By-SA License; additional terms may apply."}
+    d = {"description": "\u201cCloser\u201d is a millennial romance anthem that celebrates youth and heartbreak. It features vocals from singer-songwriter Halsey and Chainsmokers member Andrew Taggart, marking the first time The Chainsmokers sung on their own track and the first time they\u2019ve collaborated with Halsey.\n\nOn Twitter, the duo wrote about the meaning of the song:\n\nThis song is dedicated to anyone that hooked up with their EX and right after remember all the reasons why they broke up.\n\n\u201cCloser\u201d was premiered at Bonnaroo by Halsey and she later confirmed its release on her Instagram:\n\n\"You heard it tonight. My phone hasn\u2019t had service in 2 days, bonnaroo but I\u2019m pullin enough juice to inform you that @thechainsmokers and I have a BRAND NEW SONG coming out soon. Those of you who got to witness it tonight, lucky you.\nHalsey also teased the track on Twitter a week prior to the release by releasing a cropped version of the photo featured the single\u2019s art. <a href=\"http://www.last.fm/music/The+Chainsmokers/_/Closer\">Read more on Last.fm</a>. User-contributed text is available under the Creative Commons By-SA License; additional terms may apply."}
+    string = "\ud83d\udc8b\ud83d\udc8b\ud83d\udc8b\"\n"
 
     mysql_scripts = MysqlScripts('root', 'songs_track')
     # mysql_scripts.insert_folder("create_data/files")
-    track = Track(1, "mike", "dumb", 1223, "img", "lyrics", d["description"])
-    mysql_scripts.stam(track)
+    # track = Track(1, "mike", "dumb", 1223, "img", "lyrics", d["description"])
+    # mysql_scripts.stam(track)
+
+    print(string)
