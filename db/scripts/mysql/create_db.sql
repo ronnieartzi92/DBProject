baedfdc77bc00f2815a95217bd5b84067f80c7f7
Yossefy <omriykl@gmail.com>
2018-01-20 13:30:32 +0200

    Added views to create_db script

--- db/scripts/mysql/create_db.sql
+++ db/scripts/mysql/create_db.sql
@@ -99,3 +99,22 @@ CREATE TABLE tracks_isam (
 	PRIMARY KEY (id),
     FULLTEXT INDEX (lyrics, description, track_name)
 ) ENGINE = MyISAM;
+
+CREATE VIEW artists_tracks_youtubes AS
+SELECT artists.id as artist_id, artists.artist_name, artists.play_count as artist_play_count, tracks.id as track_id, tracks.track_name, tracks.img, 
+	tracks.play_count as track_play_count, youtubes.video_id, youtubes.date_published
+FROM youtubes
+INNER JOIN tracks ON youtubes.track_id = tracks.id
+INNER JOIN artists ON tracks.artist_id = artists.id;
+
+CREATE VIEW artists_newest_track AS
+SELECT DISTINCT tracks.artist_id, x.artist_name, tracks.id as track_id, tracks.track_name, tracks.img, tracks.play_count, youtubes.video_id, youtubes.date_published
+FROM youtubes
+INNER JOIN tracks ON youtubes.track_id = tracks.id
+INNER JOIN artists AS x ON tracks.artist_id = x.id
+AND youtubes.date_published = (
+	SELECT MAX(date_published) 
+    FROM youtubes
+	INNER JOIN tracks ON tracks.id = youtubes.track_id
+    WHERE x.id = tracks.artist_id
+);
