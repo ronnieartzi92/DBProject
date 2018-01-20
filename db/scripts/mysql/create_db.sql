85d48ad33608595781f1336eddd226c696147361
Mike Moldawsky <mike.moldawsky@gmail.com>
2018-01-20 21:01:24 +0200

    Query 6 Fixed

--- db/scripts/mysql/create_db.sql
+++ db/scripts/mysql/create_db.sql
@@ -108,7 +108,8 @@ INNER JOIN tracks ON youtubes.track_id = tracks.id
 INNER JOIN artists ON tracks.artist_id = artists.id;
 
 CREATE VIEW artists_newest_track AS
-SELECT DISTINCT tracks.artist_id, x.artist_name, tracks.id as track_id, tracks.track_name, tracks.img, tracks.play_count, youtubes.video_id, youtubes.date_published
+SELECT DISTINCT x.id as artist_id, x.artist_name, x.play_count as artist_play_count, tracks.id as track_id,
+    tracks.track_name, tracks.img, tracks.play_count as track_play_count, youtubes.video_id, youtubes.date_published
 FROM youtubes
 INNER JOIN tracks ON youtubes.track_id = tracks.id
 INNER JOIN artists AS x ON tracks.artist_id = x.id
