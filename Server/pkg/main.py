dac6e1644cb067a07d86f08833a55b6808b3f84f
Yossefy <omriykl@gmail.com>
2018-01-21 10:49:07 +0200

    minor

--- Server/pkg/main.py
+++ Server/pkg/main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, send_file
 
 import sys
 sys.path.append('.')
@@ -10,14 +10,13 @@ from routers.tag_router import tag_router
 from routers.artist_router import artist_router
 from routers.track_router import track_router
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
 app.secret_key = "omri22"
 
 
-@app.route('/')
-@auth
-def index(user_id):
-    return "OK! "+str(user_id)
+@app.route('/static', strict_slashes=False)
+def index():
+    return app.send_static_file('index.html')
 
 
 @app.after_request
@@ -32,4 +31,4 @@ if __name__ == "__main__":
     app.register_blueprint(tag_router)
     app.register_blueprint(artist_router)
     app.register_blueprint(track_router)
-    app.run()
+    app.run(host='0.0.0.0', port=5000)
